969118023b00888499d3fb9b3a14e8b68e8f210b
ilyass <1925619@cegepmontpetit.ca>
2022-02-16 15:12:41 -0500

    FCT activité d'inscription

--- app/src/main/java/com/example/kickmyb/MainActivity.java
+++ app/src/main/java/com/example/kickmyb/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.kickmyb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.kickmyb.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("SignIn");
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnRegisterMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kickmyb/RegisterActivity.java
+++ app/src/main/java/com/example/kickmyb/RegisterActivity.java
@@ -1,4 +1,18 @@
 package com.example.kickmyb;
 
-public class RegisterActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kickmyb.databinding.ActivityRegisterBinding;
+
+public class RegisterActivity extends AppCompatActivity {
+    private ActivityRegisterBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        setTitle("SignUp");
+
+    }
 }
