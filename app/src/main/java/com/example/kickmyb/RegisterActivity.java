16e90f74996e540bcfcc47eecedbeae4f44abb79
i <i>
2022-04-05 15:03:53 -0400

    progres bug

--- app/src/main/java/com/example/kickmyb/RegisterActivity.java
+++ app/src/main/java/com/example/kickmyb/RegisterActivity.java
@@ -4,8 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import com.example.kickmyb.databinding.ActivityRegisterBinding;
+import com.example.kickmyb.http.RetrofitUtil;
+import com.example.kickmyb.http.Service;
+
+import org.kickmyb.transfer.SigninResponse;
+import org.kickmyb.transfer.SignupRequest;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
@@ -21,8 +31,26 @@ public class RegisterActivity extends AppCompatActivity {
         binding.btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(RegisterActivity.this, HomeActivity.class);
-                startActivity(i);
+                Service service = RetrofitUtil.get();
+                SignupRequest signupRequest = new SignupRequest();
+                signupRequest.username = "ilyas";
+                signupRequest.password = "salut123";
+                service.SignUp(signupRequest).enqueue(new Callback<SigninResponse>() {
+                    @Override
+                    public void onResponse(Call<SigninResponse> call, Response<SigninResponse> response) {
+                        if (response.isSuccessful()) {
+                            Intent i = new Intent(RegisterActivity.this, HomeActivity.class);
+                            startActivity(i);
+                            Log.i("reponse",response.body().toString());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<SigninResponse> call, Throwable t) {
+
+                    }
+                });
+
             }
         });
 
--- app/src/main/java/com/example/kickmyb/http/Service.java
+++ app/src/main/java/com/example/kickmyb/http/Service.java
@@ -2,16 +2,24 @@ package com.example.kickmyb.http;
 
 import com.example.kickmyb.task;
 
+import org.kickmyb.transfer.SigninRequest;
+import org.kickmyb.transfer.SigninResponse;
+import org.kickmyb.transfer.SignupRequest;
+
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface Service {
+        @POST("/api/id/signup")
+        Call<SigninResponse> SignUp(@Body SignupRequest signupRequest);
+
         @POST("/api/id/signin")
-        //@POST("/api/id/signup")
+        Call<SigninResponse> SignIn();
 
         @GET("users/{utilisateur}/repos")
         Call<String> listReposString(@Path("utilisateur") String utilisateur);
